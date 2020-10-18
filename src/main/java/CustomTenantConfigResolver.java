761cfb413d55bb81790a016f4f16d5b092dd9a3b
Roger Xia <rogerxiaxia@hotmail.com>
2020-10-18 20:05:40 +1100

    WIP: fixed solution

--- src/main/java/CustomTenantConfigResolver.java
+++ src/main/java/CustomTenantConfigResolver.java
@@ -10,6 +10,21 @@ public class CustomTenantConfigResolver implements TenantConfigResolver {
     @Override
     public OidcTenantConfig resolve(RoutingContext context) {
 
+        //
+        // 1. docker run -p 8180:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:11.0.2
+        /*
+           2. in browser run http://localhost:8180/auth/
+
+           3. import these 2 files
+            https://github.com/quarkusio/quarkus-quickstarts/blob/master/security-openid-connect-multi-tenancy/config/default-tenant-realm.json
+            https://raw.githubusercontent.com/quarkusio/quarkus-quickstarts/master/security-openid-connect-multi-tenancy/config/default-tenant-realm.json
+
+            in the keycloak system
+            select Realm
+            click "Add Realm"
+            and then import the file
+        */
+
 
         String path = context.request().path();
         String[] parts = path.split("/");
@@ -36,6 +51,8 @@ public class CustomTenantConfigResolver implements TenantConfigResolver {
 
             config.setUserInfoPath("protocol/openid-connect/userinfo");
             config.setIntrospectionPath("protocol/openid-connect/token/introspect");
+
+           // create the client id MUST WITH Access Type confidential
             OidcTenantConfig.Credentials credentials = new OidcTenantConfig.Credentials();
             credentials.setSecret("89cbfed7-0f61-4f26-86ea-2c4053e6d881");
             config.setCredentials(credentials);
